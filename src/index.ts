a9af70d61b0cf6847d64287eec66f228edf25cd1
everton <everton@matumotion.com>
2021-01-24 23:45:57 -0300

    transparent

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import './global.css';
 
 const canvas = document.createElement('canvas');
-canvas.style.backgroundColor = 'black';
+const gl = canvas.getContext('webgl2');
+
+gl.clearColor(0, 0, 0, 0);
+gl.clear(gl.COLOR_BUFFER_BIT);
 
 
 document.body.appendChild(canvas);
